9be355646223ffa3614d4e7266dc1177dcae8947
Mustafa Muhammadi <mustafa.mohammadi@ut.ac.ir>
2022-03-22 11:34:06 +0430

    Try to Implement The Model based on Paper
    Try to implement the paper based on OHLC rather than the volume and the buy sell signal

---
+++ NewModelBasedOnPaperWithoutVolumeBuySell/custom_dataset_train_test_split.py
@@ -0,0 +1,168 @@
+import warnings
+from torch.utils.data import Dataset
+import torch
+import pandas as pd
+
+import matplotlib.pylab as plt
+
+plt.ion()  # interactive mode
+
+# ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+
+class Forex_train_Dataset(Dataset):
+    """ Reading csv files downloaded from forext sites for prediction."""
+    def __init__(self, csv_file, transform=None, seq_lenght = 5, train_size_ratio=0.8) -> None:
+        """
+        Args:
+            `csv_file` (string): Path to the csv file with annotations.
+            `root_dir` (string): Directory with all the images.
+            `transform` (callable, optional): Optional transform to be applied
+                on a sample.
+            `train_size_ratio` (float) Amount of dataset used for train-set. Default 0.8
+            
+        """
+        super().__init__()
+
+        self.seq_len = seq_lenght
+        self.transform = transform
+        self.train_size_ratio = 0.8
+        
+        # read CSV dataset and change it to torch.tensor type
+        self.dataset_csv = pd.read_csv(csv_file, encoding="UTF=8")
+        # remove date from dataset in order to prepare data for test
+        self.dataset = torch.tensor(self.dataset_csv.values[:,1:].tolist())
+        # get train samples from the dataset and set it again to the dataset
+        len_dataset = len(self.dataset)
+        
+        self.dataset = self.dataset[:int(len_dataset*self.train_size_ratio), :]
+        
+        # get dates for plotting candles if it is neccessary 
+        self.dates = self.dataset_csv.values[:int(len_dataset*self.train_size_ratio),0].tolist()
+        
+
+
+        # normalize dataset based on its features.
+        for i in range(self.dataset.shape[1] -4):   # -4 is for just normalizing input features
+            self.dataset[:,i] = (self.dataset[:,i] - self.dataset[:,i].mean()) / self.dataset[:,i].std()
+        # get headers of dataset without timestamp colums,the first columns.
+        self.headers = self.dataset_csv.columns.tolist()[1:]
+
+
+    def get_dates(self):
+            return self.dates
+
+
+    def __len__(self):
+        """
+            return the length of the dataset
+        """
+        return(len(self.dataset))
+
+    
+    def __getitem__(self, index):
+        """
+            return samples based on index and sequence length.
+            `return type`: torch.tensor
+        """
+        # print(f"samples: {self.dataset[index + self.seq_len - 1,:]}")
+
+        samples = self.dataset[index : index + self.seq_len,:-4]
+        # try to find the `close price` of the next day 
+        target_Open = self.dataset[index + self.seq_len - 1, -4]
+        target_High = self.dataset[index + self.seq_len - 1, -3]
+        target_Low = self.dataset[index + self.seq_len - 1, -2]
+        target_Close = self.dataset[index + self.seq_len - 1, -1]
+        
+        # return super().__getitem__(index)
+        # print(f"samples:\n {samples}")
+        # print(f"target:\n {target}")
+        return samples, target_Open, target_High, target_Low, target_Close 
+    
+    def header(self):
+        """
+            return the headers title of the dataset
+        """
+        return self.headers
+    
+
+class Forex_test_Dataset(Dataset):
+    """ Reading csv files downloaded from forext sites for prediction."""
+    def __init__(self, csv_file, transform=None, seq_lenght = 5, train_size_ratio=0.2) -> None:
+        """
+        Args:
+            `csv_file` (string): Path to the csv file with annotations.
+            `root_dir` (string): Directory with all the images.
+            `transform` (callable, optional): Optional transform to be applied
+                on a sample.
+            `train_size_ratio` (float) Amount of dataset used for train-set. Default 0.8
+            
+        """
+        super().__init__()
+
+        self.seq_len = seq_lenght
+        self.transform = transform
+        self.train_size_ratio = 0.8
+        
+        # read CSV dataset and change it to torch.tensor type
+        self.dataset_csv = pd.read_csv(csv_file, encoding="UTF=8")
+        # remove date from dataset in order to prepare data for test
+        self.dataset = torch.tensor(self.dataset_csv.values[:,1:].tolist())
+        
+        # get train samples from the dataset and set it again to the dataset
+        len_dataset = len(self.dataset)
+        # print(len_dataset)
+        self.dataset = self.dataset[int(len_dataset * self.train_size_ratio):, :]
+        # print(len(self.dataset))
+
+        # get dates for plotting candles if it is neccessary 
+        self.dates = self.dataset_csv.values[int(len_dataset * self.train_size_ratio):, 0].tolist()
+
+        # normalize dataset based on its features.
+        for i in range(self.dataset.shape[1] -4):   # -4 is for just normalizing input features
+            self.dataset[:,i] = (self.dataset[:,i] - self.dataset[:,i].mean()) / self.dataset[:,i].std()
+        # get headers of dataset without timestamp colums,the first columns.
+        self.headers = self.dataset_csv.columns.tolist()[1:]
+
+
+    def __len__(self):
+        """
+            return the length of the dataset
+        """
+        return(len(self.dataset))
+
+
+    def get_dates(self):
+            return self.dates
+
+
+    def __getitem__(self, index):
+        """
+            return samples based on index and sequence length.
+            `return type`: torch.tensor
+        """
+
+        samples = self.dataset[index : index + self.seq_len,:-5]
+        # try to find the `close price` of the next day 
+        target_Open = self.dataset[index + self.seq_len - 1, -4]
+        target_High = self.dataset[index + self.seq_len - 1, -3]
+        target_Low = self.dataset[index + self.seq_len - 1, -2]
+        target_Close = self.dataset[index + self.seq_len - 1, -1]
+        
+        # return super().__getitem__(index)
+        # print(f"samples:\n {samples}")
+        # print(f"target:\n {target}")
+        return samples, target_Open, target_High, target_Low, target_Close 
+    
+    def header(self):
+        """
+            return the headers title of the dataset
+        """
+        return self.headers
+    
+
+# there is no train of test dataset split
+
+# there is no train of test dataset split
---
+++ NewModelBasedOnPaperWithoutVolumeBuySell/extended_functions_full_candlestick.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from parameters import *
+import torch
+import numpy as np
+import parameters
+import mplfinance as mpf
+
+
+plt.ion()
+
+
+def plot_acc_loss(data:list, caption):
+    # Plot figure
+    plt.figure(figsize=(10,5))
+    plt.title(f"Training and Validation Loss with lr = {lr}")
+    # plt.plot(data, label="val")
+    plt.yticks(np.arange(0, 2, step=0.001))
+    plt.plot(data, label=caption)
+    plt.xlabel("iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.grid(b=True, color = 'green', linestyle = '--', linewidth = 0.5)
+    plt.show()
+    plt.pause(20)
+
+
+def plot_test_values_predicted(real_values:list, predicted_values:list,  caption_for_real_values, caption_for_predicted_values):
+    # Plot figure
+    plt.figure(figsize=(10,5),)
+    plt.title("Real test and predicted values")
+    # plt.plot(data, label="val")
+    plt.plot(real_values, label=caption_for_real_values)
+    plt.plot(predicted_values, label=caption_for_predicted_values)
+    plt.xlabel("Days")
+    plt.ylabel("Close Price")
+    plt.legend(["Real price","Predicted price"])
+    plt.show()
+
+
+def plot_price(price_list):
+    # show close prices in python plot
+    plt.ion()
+    plt.plot(price_list)
+    plt.annotate("close price", [0.,0.])
+    plt.show()
+    plt.pause(10)
+
+
+def adjust_learning_rate(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = optimizer.parag_gropus[0]['lr'] * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+# saving model parameters
+def save_model(model_Open, model_High, model_Low, model_Close, 
+                optimizer_Open, optimizer_High, Optimizer_Low, Optimizer_Close,
+                epoch, 
+                loss_Open, loss_High, loss_Low, loss_Close):
+    state_dicts = { 'model_Open_state_dict': model_Open.state_dict(),
+                    'model_Hight_state_dict': model_High.state_dict(),
+                    'model_Low_state_dict': model_Low.state_dict(),
+                    'model_Close_state_dict': model_Close.state_dict(),
+                    'optimizer_Open_state_dict': optimizer_Open.state_dict(), 
+                    'optimizer_High_state_dict': optimizer_High.state_dict(), 
+                    'optimizer_Low_state_dict': Optimizer_Low.state_dict(), 
+                    'optimizer_Close_state_dict': Optimizer_Close.state_dict(), 
+                    'epoch': epoch, 
+                    'loss_Open': loss_Open,
+                    'loss_High': loss_High,
+                    'loss_Low': loss_Low,
+                    'loss_Close': loss_Close
+                    }
+    model_state_dict_path = parameters.model_state_dict_path
+    torch.save(state_dicts, model_state_dict_path)
+
+def plot_price_chart(prices):
+    # source_csv = pd.read_csv(reader=model_state_dict_path, encoding="UTF8")
+    
+    # candles = pd.read_csv(dataset_filePath,index_col=0,parse_dates=True)
+    # dates = candles.values[int(len(candles) * 0.8):, 0].tolist()
+    candles = pd.DataFrame(prices).transpose()
+    candles.columns = ['Date', 'Open', 'High', 'Low', 'Close']
+    candles['Date'] = pd.to_datetime(candles['Date'])
+    # candles.index.name = 'Date'
+    mpf.plot(candles)
+    print("end plot chart")
+    
