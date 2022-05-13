22deb2975d1f7e77406b6fe7f2f00753ebf0f8d3
Mustafa Muhammadi <mustafa.mohammadi@ut.ac.ir>
2022-05-13 10:59:47 +0430

    .

--- Model_Full_with_Single_model/dataset_with_normalized_Holidays.py
+++ Model_Full_with_Single_model/dataset_with_normalized_Holidays.py
@@ -1,3 +1,9 @@
+"""
+
+This code is to normalized holidays with the last three days prices
+There is ability to combine this method by GAT Networks
+
+"""
 import warnings
 from torch.utils.data import Dataset
 import torch
@@ -5,7 +11,6 @@ import pandas as pd
 from datetime import datetime
 
 import matplotlib.pylab as plt
-
 plt.ion()  # interactive mode
 
 # ignore warnings
@@ -15,7 +20,7 @@ warnings.filterwarnings("ignore")
 
 class Forex_train_Dataset(Dataset):
     """ Reading csv files downloaded from forext sites for prediction."""
-    def __init__(self, csv_file, transform=None, seq_lenght = 5, train_size_ratio=0.8) -> None:
+    def __init__(self, csv_file, transform=None, seq_lenght = 5, train_size_ratio=0.8, notmalized_days_lenght:int=3) -> None:
         """
         Args:
             `csv_file` (string): Path to the csv file with annotations.
@@ -23,17 +28,18 @@ class Forex_train_Dataset(Dataset):
             `transform` (callable, optional): Optional transform to be applied
                 on a sample.
             `train_size_ratio` (float) Amount of dataset used for train-set. Default 0.8
-            
         """
+        
         super().__init__()
 
         self.seq_len = seq_lenght
         self.transform = transform
         self.train_size_ratio = 0.8
-        
+        # self.normalized_days_lenght = normalized_days_lenght
+
         # read CSV dataset and change it to torch.tensor type
         self.dataset_csv = pd.read_csv(csv_file, encoding="UTF=8")
-        
+
         """
         temp = pd.Timestamp("2020-11-25")
 
@@ -42,17 +48,17 @@ class Forex_train_Dataset(Dataset):
 
         datetime.date(temp).weekday()
         """
-        
+
         # remove date from dataset in order to prepare data for test
         self.dataset = torch.tensor(self.dataset_csv.values[:,1:].tolist())
         # get train samples from the dataset and set it again to the dataset
         len_dataset = len(self.dataset)
         # obtain trainset  by train_size_ratio
         self.dataset = self.dataset[:int(len_dataset*self.train_size_ratio), :]
-        
-        # get dates for plotting candles if it is neccessary 
+
+        # get dates for plotting candles if it is neccessary
         self.dates = self.dataset_csv.values[:int(len_dataset*self.train_size_ratio),0].tolist()
-        
+
 
         # normalize dataset based on its features.
         for i in range(self.dataset.shape[1] -4):   # -4 is for just normalizing input features
@@ -75,7 +81,7 @@ class Forex_train_Dataset(Dataset):
         """
         return(len(self.dataset))
 
-    
+
     def __getitem__(self, index):
         """
             return samples based on index and sequence length.
@@ -84,23 +90,23 @@ class Forex_train_Dataset(Dataset):
         # print(f"samples: {self.dataset[index + self.seq_len - 1,:]}")
 
         samples = self.dataset[index : index + self.seq_len,:-4]
-        # try to find the `close price` of the next day 
+        # try to find the `close price` of the next day
         target_Open = self.dataset[index + self.seq_len - 1, -4]
         target_High = self.dataset[index + self.seq_len - 1, -3]
         target_Low = self.dataset[index + self.seq_len - 1, -2]
         target_Close = self.dataset[index + self.seq_len - 1, -1]
-        
+
         # return super().__getitem__(index)
         # print(f"samples:\n {samples}")
         # print(f"target:\n {target}")
         return samples, target_Open, target_High, target_Low, target_Close #samples
-    
+
     def header(self):
         """
             return the headers title of the dataset
         """
         return self.headers
-    
+
 
 class Forex_test_Dataset(Dataset):
     """ Reading csv files downloaded from forext sites for prediction."""
@@ -112,26 +118,26 @@ class Forex_test_Dataset(Dataset):
             `transform` (callable, optional): Optional transform to be applied
                 on a sample.
             `train_size_ratio` (float) Amount of dataset used for train-set. Default 0.8
-            
+
         """
         super().__init__()
 
         self.seq_len = seq_lenght
         self.transform = transform
         self.train_size_ratio = 0.8
-        
+
         # read CSV dataset and change it to torch.tensor type
         self.dataset_csv = pd.read_csv(csv_file, encoding="UTF=8")
         # remove date from dataset in order to prepare data for test
         self.dataset = torch.tensor(self.dataset_csv.values[:,1:].tolist())
-        
+
         # get train samples from the dataset and set it again to the dataset
         len_dataset = len(self.dataset)
         # print(len_dataset)
         # self.dataset = self.dataset[int(len_dataset * self.train_size_ratio):, :]
         # print(len(self.dataset))
 
-        # get dates for plotting candles if it is neccessary 
+        # get dates for plotting candles if it is neccessary
         self.dates = self.dataset_csv.values[int(len_dataset * self.train_size_ratio):, 0].tolist()
 
         # normalize dataset based on its features.
@@ -163,23 +169,23 @@ class Forex_test_Dataset(Dataset):
         """
 
         samples = self.dataset[index : index + self.seq_len,:-4]
-        # try to find the `close price` of the next day 
+        # try to find the `close price` of the next day
         target_Open = self.dataset[index + self.seq_len - 1, -4]
         target_High = self.dataset[index + self.seq_len - 1, -3]
         target_Low = self.dataset[index + self.seq_len - 1, -2]
         target_Close = self.dataset[index + self.seq_len - 1, -1]
-        
+
         # return super().__getitem__(index)
         # print(f"samples:\n {samples}")
         # print(f"target:\n {target}")
-        return samples, target_Open, target_High, target_Low, target_Close 
-    
+        return samples, target_Open, target_High, target_Low, target_Close
+
     def header(self):
         """
             return the headers title of the dataset
         """
         return self.headers
-    
+
 
 # there is no train of test dataset split
 
